d72065568201bf7787f633248cb1a2ac72c0cd62
Billy <89958256+gobillyx@users.noreply.github.com>
2021-11-09 19:19:58 +0700

    update alamat email

--- admin/report.php
+++ admin/report.php
@@ -6,19 +6,20 @@
 
 <p class="fs-4">
 	[English] <br>
-	Thank you for using this software, if you find an error or something bad, send a message to my email gobillyx[at]gmail.com or at <a href="https://github.com/gobillyx/Dipolling/issues" target="_blank">Github Issues</a>. See you on the Next Update!
+	Thank you for using this software, if you find an error or something bad, send a message to my email gobillyx72[at]gmail.com or at <a href="https://github.com/gobillyx/Dipolling/issues" target="_blank">Github Issues</a>. See you on the Next Update!
 	<br>
 	<br>
 	[Indonesia] <br>
-	Terimakasih telah menggunakan perangkat lunak ini, jika kamu menemukan error atau sesuatu yang buruk kirim pesan surel ke gobillyx[at]gmail.com atau di <a href="https://github.com/gobillyx/Dipolling/issues" target="_blank">Github Issues</a>. Sampai jumpa lagi di pembaruan selanjutnya!
+	Terimakasih telah menggunakan perangkat lunak ini, jika kamu menemukan error atau sesuatu yang buruk kirim pesan surel ke gobillyx72[at]gmail.com atau di <a href="https://github.com/gobillyx/Dipolling/issues" target="_blank">Github Issues</a>. Sampai jumpa lagi di pembaruan selanjutnya!
 </p>
 <span>&nbsp;</span>
 <span>&nbsp;</span>
 <span>&nbsp;</span>
 <small>
 <span class="mt-5 d-block text-secondary">&copy;<?php echo date('Y'); ?> Dipolling - (Under MIT License) 
-By Febry Billiyagi a.k.a gobillyx</span>
-<span class="text-secondary d-block mt-1">Follow me on instagram <a href="https://instagram.com/gobillyx" class="text-decoration-none">@gobillyx72</a></span>
+By Febry Billiyagi a.k.a gobillyx
+</span>
+<span class="text-secondary d-block mt-1">Follow me on instagram <a href="https://instagram.com/gobillyx" class="text-decoration-none">@gobillyx</a></span>
 </small>
 
 <?php require 'template/main.php' ?>
