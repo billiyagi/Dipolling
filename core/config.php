55d56b2f8e11b33e9415c321f0610ba7a1d57e72
Billy <89958256+gobillyx@users.noreply.github.com>
2021-12-14 18:12:38 +0700

    Update config.php

--- core/config.php
+++ core/config.php
@@ -1,6 +1,6 @@
 <?php
-$db_host_name = '';
-$db_username = '';
-$db_password = '';
-$db_name = '';
-?>
+     $db_host_name = '';
+     $db_username = '';
+     $db_password = '';
+     $db_name = '';
+     ?>
