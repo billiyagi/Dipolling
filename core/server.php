a378898451e3e4f0764d154f86151a936aebb483
Billy <89958256+gobillyx@users.noreply.github.com>
2021-12-14 18:09:52 +0700

    Refactoring Instalation

--- core/server.php
+++ core/server.php
@@ -1,33 +1,17 @@
 <?php
-require 'config.php';
-require 'class.php';
+require_once 'config.php';
+require_once 'class/DB.php';
+require_once 'function.php';
 error_reporting(0);
 
-$dipolling = new Dipolling($db_host_name, $db_username, $db_password, $db_name);
-$notify = new Notification($db_host_name, $db_username, $db_password, $db_name);
-$show_polling = new dipollingTable($db_host_name, $db_username, $db_password, $db_name);
-
-if($dipolling->connect_errno){
-    $db_error = true; //result true / 1
-}else{
-    $db_error = false;
-}
+// Init database Connection
+$DB = new DB( $db_host_name, $db_username, $db_password, $db_name );
 
 // cek instalasi
-if ($db_error && empty($db_host_name) && empty($db_username) && empty($db_name) ){
-    session_start();
-    $_SESSION['instalation'] = 'database';
-    header("Location: instalation?step=database");
-}elseif( $db_error || empty($db_host_name) || empty($db_username) || empty($db_name) ){
-    session_start();
-    $_SESSION['instalation'] = 'error';
-    header("Location: instalation?step=error");
-}elseif(isset($_SESSION)){
-    //jika ada session dan session itu instalasi hapus sessionnya
-    if ($_SESSION['instalation']) {
-        session_start();
-        $_SESSION = [];
-        session_unset();
-        session_destroy();
-    }
+
+
+if ( $DB->connect_errno() != 0 || empty( $db_host_name ) || empty( $db_username ) || empty( $db_name )  ){
+     session_start();
+     $_SESSION['install'] = true;
+     header("Location: install");
 }
